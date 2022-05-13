680d7a3d9a357a2fc2289383b66725c605a0a303
Aman Kumar <amanofficial16@gmail.com>
2022-05-13 16:52:02 +0530

    modified files containing sensitive data

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBPNwJ6w3DcuO7UQlElfADDWdNFEDVMzN8",
-    authDomain: "crypto-hunter0710.firebaseapp.com",
-    projectId: "crypto-hunter0710",
-    storageBucket: "crypto-hunter0710.appspot.com",
-    messagingSenderId: "197592104911",
-    appId: "1:197592104911:web:8be56fd943e039aea61cba"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
   export default firebaseConfig;
